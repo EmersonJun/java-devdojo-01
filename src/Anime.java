186618e637ed75d1a26292b8cbc69ad0422fa760
Emerson <emersonnjunior2006@gmail.com>
2025-05-08 13:34:45 -0300

    java devdojo 01

---
+++ src/Anime.java
@@ -0,0 +1,14 @@
+public class Anime {
+    private String nome;
+    private int[] episodios;
+
+    public Anime(){
+        episodios = new int[100];
+        for(int i = 0; i< episodios.length; i++){
+            episodios[i] = i+1;
+        }
+        for(int episodios : this.episodios){
+            System.out.println(episodios + " ");
+        }
+    }
+}
---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Anime anime = new Anime();
+    }
+}
