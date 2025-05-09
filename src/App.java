e15b2b8c2cdf104ff129c9bc71baede211ea4e77
Emerson <emersonnjunior2006@gmail.com>
2025-05-09 14:27:25 -0300

    associacao associacao unidimencional para um e muitos

--- src/App.java
+++ src/App.java
@@ -1,12 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Jogador jogador1 = new Jogador("pele");
-        Jogador jogador2 = new Jogador("neymar");
-        Jogador jogador3 = new Jogador("kaka");
-        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+        Time time = new Time("selecao br");
 
-        for (Jogador jogador : jogadores) {
-            jogador.imprime();
-        }
+        jogador1.setTime(time);
+        jogador1.imprime();
+        // Jogador jogador2 = new Jogador("neymar");
+        // Jogador jogador3 = new Jogador("kaka");
+        //  Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+        //  for (Jogador jogador : jogadores) {
+        //      jogador.imprime();
+        //  }
     }
 }
--- src/Jogador.java
+++ src/Jogador.java
@@ -1,16 +1,29 @@
+
 public class Jogador {
     private String nome;
+    private Time time;
 
     public Jogador(String nome) {
         this.nome = nome;
     }
 
     public void imprime(){
-        System.out.println("Nome: "+getNome());
+        System.out.println("Nome: "+this.nome);
+        if (time != null) {
+            System.out.println("time: "+time.getNome());
+        }
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) { 
+        this.time = time;
     }
 
     public void setNome(String nome) {
