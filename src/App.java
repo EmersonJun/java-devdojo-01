50cf80b4ceb497827a5776a7d3606dccc440fca4
Emerson <emersonnjunior2006@gmail.com>
2025-05-12 11:28:36 -0300

    exercice

--- src/App.java
+++ src/App.java
@@ -1,16 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Jogador jogador = new Jogador("cafu");
-        Jogador pele = new Jogador("pele");
-        Time brasil = new Time("brasil");
-        Jogador[] jogadores = {jogador, pele};
+        Aluno pedro = new Aluno("pedro", 10);
+        Aluno paulo = new Aluno("paulo", 11);
+        
+        Professor jonas = new Professor("jonas", "matematica");
+        Local curitiba = new Local("curitiba");
 
-        jogador.setTime(brasil);
-        brasil.setJogadores(jogadores);
-
-        System.out.println("--------------------------");
-        jogador.impime();
-        System.out.println("--------------------------");
-        brasil.imprinme();
+        Aluno[] alunos = {pedro, paulo};
+        Seminario palestra = new Seminario("palestra", alunos, curitiba, jonas);
+        palestra.imprime();
     }
 }
