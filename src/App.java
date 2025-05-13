903be29e954276c4c394ab3a0c7c7e61f55e1f48
Emerson <emersonnjunior2006@gmail.com>
2025-05-13 10:51:11 -0300

    Sobrescrita do método toString

--- src/App.java
+++ src/App.java
@@ -1,19 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        // Endereco endereco = new Endereco();
-        // endereco.setCep("123456789");
-        // endereco.setRua("braba");
-        // Pessoa pessoa = new Pessoa("paulo");
-        // pessoa.setCpf("987654321");
-        // pessoa.setEndereco(endereco);
-        // pessoa.imprime();
-
-        // Funcionario funcionario = new Funcionario("jonas");
-        // funcionario.setCpf("23141526");
-        // funcionario.setEndereco(endereco);
-        // funcionario.setSalario(1200);
-        // System.out.println("--------------------------------");
-        // funcionario.imprime();
-        Funcionario funcionario = new Funcionario("carlos");
+        Anime anime = new Anime("dr stone");
+        System.out.println(anime);
     }
 }
